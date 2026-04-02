ec393723e8423aa39cf52e630d99719c69629ad7
MohammedNiyamath40 <mohammedniyamath40@gmail.com>
2026-04-02 17:48:37 +0530

    Instance Variable with this keyword 1-4-2026

---
+++ ContactRunner.java
@@ -0,0 +1,18 @@
+class ContactRunner{
+	public static void main(String[] contact){
+		Contact person1 = new Contact("Mohammed", "Niyamath", 7975106965l);
+		person1.contactSave();
+		Contact person2 = new Contact("Arziya", "Banu", 7676686629l);
+		person2.contactSave();
+		Contact person3 = new Contact("Shaniaymeth", "Brother", 9980654977l);
+		person3.contactSave();
+		Contact person4 = new Contact("Syed", "Zuhaib", 8676456776l);
+		person4.contactSave();
+		Contact person5 = new Contact("Shafiya", "Banu", 9845678921l);
+		person5.contactSave();
+		Contact person6 = new Contact("Muyeez", "Baig", 9845982375l);
+		person6.contactSave();
+		Contact person7 = new Contact("Tasleem", "Banu", 7259328786l);
+		person7.contactSave();
+	}
+}
